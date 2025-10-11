f452ea765cd8638aedd1eb6015317790d3b030f5
GarrettT <spinnytea@gmail.com>
2025-10-11 16:40:05 -0400

    parse eml file

--- app/parse/mail.py
+++ app/parse/mail.py
@@ -1,4 +1,31 @@
 import mailbox
+from email import policy
+from email.parser import BytesParser
+
+
+def parse_eml_file(eml_file_path):
+    """
+    Parses an Eml file, converts the email in some kind of data object
+
+    eml_file_path: filepath of eml file
+    return list of dicts
+        id: some arbitrary value to tell them apart
+        date_raw: date from email
+        body_html: html content of email
+        body_text: text content of email
+    """
+    transactions = []
+
+    try:
+        with open(eml_file_path, "rb") as fp:
+            message = BytesParser(policy=policy.default).parse(fp)
+        transactions.append(get_transaction_from_message(0, message))
+    except FileNotFoundError:
+        print(f"Error: Mbox file not found at {eml_file_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    return transactions
 
 
 def parse_mbox_file(mbox_file_path):
@@ -13,63 +40,63 @@ def parse_mbox_file(mbox_file_path):
         body_text: text content of email
     """
     transactions = []
+
     try:
         # Open the Mbox file
         with MboxReader(mbox_file_path) as mbox:
             # Iterate through each message in the Mbox file
             for i, message in enumerate(mbox):
-                trans = {}
-                trans["idx"] = i
-
-                # Extract header information
-                # data["from"] = message["from"]
-                # data["to"] = message["to"]
-                # data["subject"] = message["subject"]
-                trans["date_raw"] = message["date"]
-
-                trans["id"] = f"{i}@{trans['date_raw']}"
-
-                # Extract and print the email body (plain text part)
-                if message.is_multipart():
-                    for part in message.walk():
-                        ctype = part.get_content_type()
-                        cdispo = str(part.get("Content-Disposition"))
-
-                        # trans.setdefault("warning", []).append(
-                        #     f"Message: {trans['id']}, Content-Type: {ctype}, Content-Disposition: {cdispo}"
-                        # )
-
-                        # Only consider text parts, not attachments
-                        if (ctype == "text/html") and "attachment" not in cdispo:
-                            if "body_html" in trans:
-                                trans.setdefault("warning", []).append(
-                                    "Multiple html bodies in message"
-                                )
-                            try:
-                                trans["body_html"] = part.get_payload(
-                                    decode=True
-                                ).decode()
-                            except UnicodeDecodeError:
-                                trans.setdefault("warning", []).append(
-                                    f"Body could not be decoded (multipart, {ctype})"
-                                )
-                            break  # Stop after finding the first plain text part
-                else:
-                    try:
-                        trans["body_text"] = message.get_payload(decode=True).decode()
-                    except UnicodeDecodeError:
-                        trans.setdefault("warning", []).append(
-                            "Body could not be decoded"
-                        )
-
-                transactions.append(trans)
-
+                transactions.append(get_transaction_from_message(i, message))
     except FileNotFoundError:
         print(f"Error: Mbox file not found at {mbox_file_path}")
     except Exception as e:
         print(f"An error occurred: {e}")
 
     return transactions
+
+
+def get_transaction_from_message(i, message):
+    trans = {}
+    trans["idx"] = i  # FIXME not unique when across files
+
+    # Extract header information
+    # data["from"] = message["from"]
+    # data["to"] = message["to"]
+    # data["subject"] = message["subject"]
+    trans["date_raw"] = message["date"]
+
+    trans["id"] = f"{i}@{trans['date_raw']}"
+
+    # Extract and print the email body (plain text part)
+    if message.is_multipart():
+        for part in message.walk():
+            ctype = part.get_content_type()
+            cdispo = str(part.get("Content-Disposition"))
+
+            # trans.setdefault("warning", []).append(
+            #     f"Message: {trans['id']}, Content-Type: {ctype}, Content-Disposition: {cdispo}"
+            # )
+
+            # Only consider text parts, not attachments
+            if (ctype == "text/html") and "attachment" not in cdispo:
+                if "body_html" in trans:
+                    trans.setdefault("warning", []).append(
+                        "Multiple html bodies in message"
+                    )
+                try:
+                    trans["body_html"] = part.get_payload(decode=True).decode()
+                except UnicodeDecodeError:
+                    trans.setdefault("warning", []).append(
+                        f"Body could not be decoded (multipart, {ctype})"
+                    )
+                break  # Stop after finding the first plain text part
+    else:
+        try:
+            trans["body_text"] = message.get_payload(decode=True).decode()
+        except UnicodeDecodeError:
+            trans.setdefault("warning", []).append("Body could not be decoded")
+
+    return trans
 
 
 class MboxReader:
--- test/parse/test_mail.py
+++ test/parse/test_mail.py
@@ -1,9 +1,33 @@
 import unittest
 
-from app.parse.mail import parse_mbox_file
+from app.parse.mail import parse_eml_file, parse_mbox_file
 
 
-class TestParseMail(unittest.TestCase):
+class TestParseEmail(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.transactions = parse_eml_file("data/test_data/test_simple.eml")
+
+    def test_count(self):
+        self.assertEqual(len(self.transactions), 1)
+
+    def test_all_keys(self):
+        for trans in self.transactions:
+            self.assertEqual(
+                sorted(trans.keys()),
+                ["body_html", "date_raw", "id", "idx"],
+                f"bad keys for transaction {trans['id']}",
+            )
+
+    def test_one(self):
+        trans = self.transactions[0]
+        self.assertEqual(sorted(trans.keys()), ["body_html", "date_raw", "id", "idx"])
+        self.assertEqual(trans["id"], "0@Wed, 08 Oct 2025 00:23:10 +0000")
+        self.assertEqual(trans["idx"], 0)
+        self.assertEqual(trans["date_raw"], "Wed, 08 Oct 2025 00:23:10 +0000")
+
+
+class TestParseMailbox(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self.transactions = parse_mbox_file("data/test_data/test_simple.mbox")
